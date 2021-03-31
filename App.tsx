e02d1483e3aa1c88a4b5c0498c4a54f5f02f7ef2
Christian Hess <christian.hess.invillia@tembici-consult.com>
2021-03-30 21:47:32 -0300

    add teste de exemplo do react testing library

--- App.tsx
+++ App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import {SafeAreaView, useColorScheme} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import SharkSVG from './src/assets/icons/profile/001-shark.svg';
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView>
+    <SafeAreaView testID="app-safe-area-view">
       <SharkSVG color="black" />
     </SafeAreaView>
   );
