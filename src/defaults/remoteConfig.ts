e4aeda472a384d2de0810df03327ea04e2472f21
Christian Hess <christian.hess.invillia@tembici-consult.com>
2021-04-10 21:27:12 -0300

    Iniciado

---
+++ src/defaults/remoteConfig.ts
@@ -0,0 +1,9 @@
+interface remoteConfigDefaultParams {
+  test_feature: string;
+}
+
+const remoteConfigDefaultValues: remoteConfigDefaultParams = {
+  test_feature: 'Valor',
+};
+
+export default remoteConfigDefaultValues;
