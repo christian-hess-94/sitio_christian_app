be7e91016ac1a285287e40b21d94e8aeee4f02ee
Christian Hess <christian.hess.invillia@tembici-consult.com>
2021-04-10 22:07:46 -0300

    Iniciar importação

--- src/screens/splash/splash.screen.tsx
+++ src/screens/splash/splash.screen.tsx
@@ -3,7 +3,7 @@ import {AppLogo, SplashScreenContainer} from './splash.styles';
 import {StackScreenNames} from '..';
 import {StackScreenProps as SSP} from '@react-navigation/stack';
 import {Button} from 'react-native-paper';
-
+import remoteConfig from '@react-native-firebase/remote-config';
 export interface SplashScreenProps {}
 
 const SplashScreen: React.FC<SSP<StackScreenNames, 'Splash'>> = ({
