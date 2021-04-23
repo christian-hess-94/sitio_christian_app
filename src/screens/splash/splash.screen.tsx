b6ca3e47990f41dd1ba58f8eee09b14170660845
Christian Hess <christian.hess.invillia@tembici-consult.com>
2021-04-22 21:41:55 -0300

    Animação suave do loading

--- src/screens/splash/splash.screen.tsx
+++ src/screens/splash/splash.screen.tsx
@@ -1,54 +1,65 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from 'react';
-import {
-  AppLogo,
-  ConfigProgressBar,
-  SplashScreenContainer,
-} from './splash.styles';
+import {AppLogo, SplashScreenContainer} from './splash.styles';
 import {StackScreenNames} from '..';
 import {StackScreenProps as SSP} from '@react-navigation/stack';
-import {AsyncTask, execTasks} from '../../utils/asyncTask.util';
+import {AsyncTask, execTask} from '../../utils/asyncTask.util';
 import {
   fetchAndActivateConfig,
   setRemoteConfigDefaults,
 } from '../../tasks/remoteConfig.tasks';
+import ProgressBar from '../../components/progressBar';
 export interface SplashScreenProps {}
 
 const SplashScreen: React.FC<SSP<StackScreenNames, 'Splash'>> = ({
   navigation: {reset},
 }) => {
-  const [taskIndex, setTasksDone] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [taskIndex, setTaskIndex] = useState(0);
   const tasks: AsyncTask[] = [
     {
       name: 'Set Remote Config Defaults',
       task: setRemoteConfigDefaults,
       isAsync: true,
-      onComplete: () => setTasksDone(taskIndex + 1),
+      onComplete: () => {
+        setTaskIndex(taskIndex + 1);
+        setProgress(progress + 100 / tasks.length);
+      },
     },
     {
       name: 'Fetch and Activate Config Defaults',
       task: fetchAndActivateConfig,
       isAsync: true,
-      onComplete: () => setTasksDone(taskIndex + 1),
+      onComplete: () => {
+        setTaskIndex(taskIndex + 1);
+        console.log('task length', tasks.length);
+        setProgress(progress + 100 / tasks.length);
+      },
     },
   ];
+
   const [tasksToDo] = useState(tasks.length);
 
   useEffect(() => {
+    console.log(taskIndex);
+    console.log(progress);
     if (taskIndex < tasksToDo) {
-      execTasks({
-        tasks,
-        taskIndex,
-      });
+      execTask(tasks[taskIndex]);
     }
   }, [taskIndex]);
+
+  const initApp = () => {
+    if (taskIndex >= tasks.length) {
+      reset({index: 0, routes: [{name: 'Login'}]});
+    }
+  };
   return (
     <SplashScreenContainer>
       <AppLogo />
-      <ConfigProgressBar
-        total={tasksToDo}
+      <ProgressBar
         taskIndex={taskIndex}
-        onProgressCompleted={() => reset({index: 0, routes: [{name: 'Login'}]})}
+        tasksToDo={tasksToDo}
+        onAnimationEnd={initApp}
       />
     </SplashScreenContainer>
   );
--- src/screens/splash/splash.styles.ts
+++ src/screens/splash/splash.styles.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components/native';
 import {LogoSVG} from '../../assets';
-import {ProgressBar} from 'react-native-paper';
-import {Dimensions} from 'react-native';
 export const SplashScreenContainer = styled.SafeAreaView`
   background-color: ${({theme}) => theme.colors.primary};
   width: 100%;
@@ -15,20 +13,3 @@ export const AppLogo = styled(LogoSVG).attrs(({theme}) => ({
   width: 240,
   color: theme.colors.secondary,
 }))``;
-
-interface ConfigProgressBarProps {
-  taskIndex: number;
-  total: number;
-  onProgressCompleted: () => void;
-}
-export const ConfigProgressBar = styled(
-  ProgressBar,
-).attrs<ConfigProgressBarProps>(({total, taskIndex, onProgressCompleted}) => ({
-  progress: taskIndex / total,
-  color: '#fff',
-  onAnimationEnd: taskIndex === total && onProgressCompleted(),
-}))<ConfigProgressBarProps>`
-  width: ${Dimensions.get('screen').width * 0.7}px;
-  height: ${({theme}) => theme.spacings.medium}px;
-  border-radius: ${({theme}) => theme.spacings.medium}px;
-`;
