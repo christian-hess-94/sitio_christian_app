e6552b6dd3222b2884e2800e07835ff1c926c98f
Christian Hess <christian.hess.invillia@tembici-consult.com>
2021-04-25 17:25:35 -0300

    Adicionado subscriber de authenticação

---
+++ src/screens/panel/panel.screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StackScreenProps as SSP} from '@react-navigation/stack';
+import {StackScreenNames} from '..';
+import {Text} from 'react-native';
+
+interface PanelScreenProps {}
+
+const PanelScreen: React.FC<SSP<StackScreenNames, 'Panel'>> = () => {
+  return <Text>Panel Screen</Text>;
+};
+
+export default PanelScreen;
